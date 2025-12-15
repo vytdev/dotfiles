2379fba420cdd3572328787e3bea75e3d7343f84
Vincent Yanzee J. Tan <vytdev@gmail.com>
2025-12-15 06:43:20 +0000

    feat(nvim): add lsp signature help

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -77,6 +77,7 @@ cmp.setup{
   }),
 
   sources = cmp.config.sources({
+    { name = 'nvim_lsp_signature_help', },
     { name = 'nvim_lua', },  -- neovim lua api
     { name = 'nvim_lsp', },
     { name = 'async_path', },
--- nvim/lua/plugins/lang.lua
+++ nvim/lua/plugins/lang.lua
@@ -33,6 +33,7 @@ return {
         dependencies = { 'rafamadriz/friendly-snippets', },
       },
       'saadparwaiz1/cmp_luasnip',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-nvim-lua',
