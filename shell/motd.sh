9e2a91a2d58b929d2bb68afcad99c3819b13ea75
Vincent Yanzee J. Tan <vytdev@gmail.com>
2025-12-21 22:58:56 +0000

    fix(shell): broader compat for escape seqs

--- shell/motd.sh
+++ shell/motd.sh
@@ -13,22 +13,22 @@ if [ ! -e "$HOME/.hushlogin" ]; then
   if [ -n "$colorize" ]; then
 
     # a green-colored block
-    b='\e[38;5;10m\e[48;5;10m..\e[0m'
+    b='\033[38;5;10m\033[48;5;10m..\033[0m'
 
     # the colored message
-    echo
-    echo -e "                    \e[1mHi, \e[32m$(whoami)\e[39m!\e[0m"
-    echo
-    echo -e "    $b$b    $b$b    A legend once said:"
-    echo -e "    $b$b    $b$b"
-    echo -e "        $b$b          For what is a man, what has he got"
-    echo -e "      $b$b$b$b        If not himself, then he has naught"
-    echo -e "      $b    $b        To say the things he truly feels"
-    echo -e "                      And not the words of one who kneels"
-    echo -e "                      The record shows, I took the blows"
-    echo -e "                      And did it, My Way!"
-    echo
-    echo -e "                      \e[1mFrank Sinatra\e[0m"
+    printf "\n"
+    printf "                    \033[1mHi, \033[32m$(whoami)\033[39m!\033[0m\n"
+    printf "\n"
+    printf "    $b$b    $b$b    A legend once said:\n"
+    printf "    $b$b    $b$b\n"
+    printf "        $b$b          For what is a man, what has he got\n"
+    printf "      $b$b$b$b        If not himself, then he has naught\n"
+    printf "      $b    $b        To say the things he truly feels\n"
+    printf "                      And not the words of one who kneels\n"
+    printf "                      The record shows, I took the blows\n"
+    printf "                      And did it, My Way!\n"
+    printf "\n"
+    printf "                      \033[1mFrank Sinatra\033[0m\n"
 
     unset b
 
@@ -36,7 +36,7 @@ if [ ! -e "$HOME/.hushlogin" ]; then
     cnt=0
     while [ "$cnt" -lt 16 ]; do
       [ $((cnt % 8)) -eq 0 ] && printf '\n                    '
-      printf '\e[38;5;%sm\e[48;5;%sm..\e[0m' "$cnt" "$cnt"
+      printf '\033[38;5;%sm\033[48;5;%sm..\033[0m' "$cnt" "$cnt"
       cnt=$((cnt + 1))
     done
     printf '\n\n'
--- shell/prompt.sh
+++ shell/prompt.sh
@@ -9,7 +9,7 @@ esac
 
 # custom prompt
 cust_ncol='\u@\h:\w\$ '
-cust_wcol='\[\e[0m\] \[\e[32m\]󰍳  \w\[\e[0m\] '
+cust_wcol='\[\033[0m\] \[\033[32m\]󰍳  \w\[\033[0m\] '
 
 [ -n "$colorize" ] && PS1="$cust_wcol" || PS1="$cust_ncol"
 
