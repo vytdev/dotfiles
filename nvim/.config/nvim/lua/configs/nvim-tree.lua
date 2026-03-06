21717ee80975b788da6a12f17a4461a7f1954d43
Vincent Yanzee J. Tan <numsqx@proton.me>
2026-03-06 18:37:20 +0800

    fix(nvim): bring back borders

--- nvim/.config/nvim/lua/configs/nvim-tree.lua
+++ nvim/.config/nvim/lua/configs/nvim-tree.lua
@@ -1,30 +1,29 @@
 require'nvim-tree'.setup{
   diagnostics = { enable = false, },
 
+  sort = {
+    sorter = 'case_sensitive',
+  },
+
   view = {
     side = 'right',
+    width = 30,
   },
 
   renderer = {
     root_folder_label = false,
+
     icons = {
       glyphs = {
-        default = '󰈔',
-        symlink = '󱅷',
         folder = {
           arrow_open = '󰅀',
           arrow_closed = '󰅂',
-          default = '󰉋',
-          open = '󰝰',
-          empty = '󰉖',
-          empty_open = '󰷏',
-          symlink = '󱧮',
         },
       },
       -- hide git status icons
       show = {
         git = false,
       },
-    },
-  },
+    }
+  }
 }
