b271da1a0c3061e7a12187abd78859dabbd461cb
Vincent Yanzee J. Tan <numsqx@proton.me>
2026-03-06 10:42:27 +0800

    fix(nvim): autocmd for filetype reload

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,11 +1,23 @@
 -- workaround for the first file not being highlighted
-vim.api.nvim_create_autocmd('BufEnter', {
-  callback = function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local name = vim.api.nvim_buf_get_name(bufnr)
-    local ft = vim.bo[bufnr].filetype
-    if name ~= '' and ft == '' then
-      vim.schedule(vim.cmd.edit)
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function(args)
+    local bufnr = args.buf
+
+    -- skip scratch buffers
+    -- skip unloaded buffers
+    -- skip other buftypes
+    -- reload to set the filetype
+    if  vim.fn.bufname(bufnr) ~= '' and
+        vim.api.nvim_buf_is_loaded(bufnr) and
+        vim.bo[bufnr].buftype == '' and
+        vim.bo[bufnr].filetype == ''
+    then
+      vim.schedule(function()
+        vim.api.nvim_buf_call(bufnr, function()
+          vim.cmd('silent! edit')
+        end)
+      end)
     end
+
   end
 })
