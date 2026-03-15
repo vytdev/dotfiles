dd88501b4ca32d8804ee5397df5a36ebba946365
Vincent Yanzee J. Tan <numsqx@proton.me>
2026-03-15 22:07:35 +0800

    fix: use default clangd lspconfig

--- nvim/.config/nvim/lua/numsqx/lsp/clangd.lua
+++ nvim/.config/nvim/lua/numsqx/lsp/clangd.lua
@@ -1,10 +1,3 @@
 -- C, C++, etc.
 
-vim.lsp.config('clangd', {
-  cmd = {
-    'clangd', '--clang-tidy', '--completion-style=detailed',
-    '--header-insertion=never',
-  }
-})
-
 vim.lsp.enable'clangd'
