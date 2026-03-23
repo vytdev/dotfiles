3332699a8b0f17ff68545b37dd829d866c4df694
Vincent Yanzee J. Tan <numsqx@proton.me>
2026-03-23 22:21:34 +0800

    fix: make motd not dependent to .hushlogin

--- custom/.custom/pref/motd.sh
+++ custom/.custom/pref/motd.sh
@@ -1,22 +1,18 @@
 # Message-Of-The-Day
 #
 
-# respect .hushlogin file
-if [ ! -e "$HOME/.hushlogin" ]; then
+crun ansi # colors and stuff
 
-  crun ansi # colors and stuff
+# a blue-colored block
+b="${cfg_blue}${cbg_blue}##${crset}"
 
-  # a blue-colored block
-  b="${cfg_blue}${cbg_blue}##${crset}"
+# the colored message
+printf "\n"
+printf "  $b  $b  $b$b$b  ${cbold}Hi, ${cfg_blue}$USER${cfrs}!${crset}\n"
+printf "  $b  $b    $b\n"
+printf "  $b$b$b    $b    ${cbold}Touch grass.${crset}\n"
+printf "  $b  $b    $b\n"
+printf "  $b  $b  $b$b$b\n"
+printf "\n"
 
-  # the colored message
-  printf "\n"
-  printf "  $b  $b  $b$b$b  ${cbold}Hi, ${cfg_blue}$USER${cfrs}!${crset}\n"
-  printf "  $b  $b    $b\n"
-  printf "  $b$b$b    $b    ${cbold}Touch grass.${crset}\n"
-  printf "  $b  $b    $b\n"
-  printf "  $b  $b  $b$b$b\n"
-  printf "\n"
-
-  unset b
-fi
+unset b
