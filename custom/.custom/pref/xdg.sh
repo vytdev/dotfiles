8f012e15601ffa1267171e601b567b7051c4b1a6
Vincent Yanzee J. Tan <numsqx@proton.me>
2026-03-25 15:59:34 +0800

    fix: *always* add ~/.local/bin to PATH

--- custom/.custom/pref/xdg.sh
+++ custom/.custom/pref/xdg.sh
@@ -7,7 +7,7 @@ export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CONFIG_DIRS="/etc/xdg"
 
-[ -d "$HOME/.local/bin" ] && caddpath "$HOME/.local/bin"
+caddpath "$HOME/.local/bin"
 
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
