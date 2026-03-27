0a3ec5b9ed22e84852d3b7deff769e6c23032626
Vincent Yanzee J. Tan <numsqx@proton.me>
2026-03-27 20:20:09 +0800

    style: reorganize env.sh again

--- custom/.custom/pref/env.sh
+++ custom/.custom/pref/env.sh
@@ -3,24 +3,28 @@
 
 # locale
 export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 # static GPG home
 export GNUPGHOME="$HOME/.gnupg"
 
 # additional user-defined binary paths
-[ -d "$HOME/bin" ] && caddpath "$HOME/bin"
+test -d "$HOME/bin" && caddpath "$HOME/bin"
 #... xdg bin path is also set (see pref/xdg.sh)
 
-# editor and pager
-export VISUAL=nvim
+# editor
 export EDITOR=nvim
+export VISUAL=$EDITOR
+
+# pager
 export PAGER=less
 export LESS='-R'
 
-# git editor and pager
-export GIT_EDITOR="$EDITOR"
-export GIT_PAGER="$PAGER"
+# git stuff
+export GIT_PAGER=$PAGER
+export GIT_EDITOR=$EDITOR
 
-# set default man path
+# man stuff
+export MANPAGER=$PAGER
 export MANPATH
 test -z "$MANPATH" && MANPATH=$(manpath)
