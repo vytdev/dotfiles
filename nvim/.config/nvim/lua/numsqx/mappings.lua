cfcba6d0be934a9fde81b27d8d703436488e805f
Vincent Yanzee J. Tan <numsqx@proton.me>
2026-03-28 20:42:15 +0800

    rm: remove window-split terminal

--- nvim/.config/nvim/lua/numsqx/mappings.lua
+++ nvim/.config/nvim/lua/numsqx/mappings.lua
@@ -6,6 +6,13 @@ map('i', 'jk', '<esc>')
 map('n', ';', ':')
 map('n', '<esc>', '<cmd>noh<cr>')
 
+map('t', '<c-x>', '<c-\\><c-n>')
+map('n', '<leader>b', function()
+  vim.cmd('terminal ' .. vim.o.shell)
+  vim.api.nvim_buf_set_name(0, 'Terminal')
+  vim.cmd('startinsert')
+end)
+
 -- window nav
 map('n', '<c-h>', '<c-w>h', { desc = 'switch window left' })
 map('n', '<c-j>', '<c-w>j', { desc = 'switch window down' })
@@ -14,8 +21,3 @@ map('n', '<c-l>', '<c-w>l', { desc = 'switch window right' })
 
 -- nvim-tree
 map('n', '<c-n>', '<cmd>NvimTreeToggle<cr>', { desc = 'nvim-tree toggle' })
-
--- terminal
-map('t', '<c-x>', '<c-\\><c-n>', { desc = 'terminal escape terminal mode' })
-map({ 'n', 't' }, '<a-t>', require'numsqx.terminal'.toggle,
-    { desc = 'terminal toggle terminal' })
